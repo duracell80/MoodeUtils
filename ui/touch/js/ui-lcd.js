3ee2c82311b76c5351574f4464d298f7e6741d25
duracell80 <lee.jordan@gmail.com>
2020-05-12 00:23:16 -0500

    Cool stuff happening here

---
+++ ui/touch/js/ui-lcd.js
@@ -0,0 +1,20 @@
+function currentTime(lcdid) {
+  var date = new Date(); /* creating object of Date class */
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById(lcdid).innerText = hour + ":" + min; /* adding time to the div */
+    var t = setTimeout(function(){ currentTime(lcdid) }, 1000); /* setting timer */
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  }
+  else {
+    return k;
+  }
+}
